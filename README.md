aa1a715ee248ef7142790ead4d8adadf688da4c5
Steve Jenson <stevej@buoyant.io>
2017-03-29 11:55:58 -0700

    Reads stdin and outputs the FNV-1a 64-bit hash value as a decimal number.

---
+++ README.md
@@ -0,0 +1,3 @@
+Reads stdin and outputs the FNV-1a 64-bit hash value as a decimal number.
+
+Licensed under the Apache 2.0 License
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+// Read from stdin, output the FNV-1a 64-bit hash value as a decimal number.
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	bytes, err := ioutil.ReadAll(reader)
+	if err != nil {
+		log.Panicf("could not read bytes from stdin %s", err)
+	}
+
+	hasher := fnv.New64a()
+	_, err2 := hasher.Write(bytes)
+	if err2 != nil {
+		log.Panicf("could not hash values %s", err)
+	}
+
+	sum := hasher.Sum64()
+	fmt.Printf("%d\n", sum)
+}
